dad34b762669788447bc83e57d8d36ffdc209e13
mullinst <mullinst2011@gmail.com>
2016-04-20 20:21:09 -0400

    feat: Added separate file from database functions.

---
+++ vagrant/src/database_functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Robert King, Jacky Liang, Troy Mullins, and Thomas Norby
+# Licensed under MIT
+# (https://github.com/Mullinst/SER-FP/blob/master/LICENSE)
+#
+# database_functions.py -- 
+
+import psycopg2
+
+
+def connect(database_name="tournament"):
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Error connecting to the database: {}".format(database_name))
