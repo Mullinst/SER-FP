80575cb24ace9fbbce7a2c28e62ce35c6a8e3445
mullinst <mullinst2011@gmail.com>
2016-04-26 11:10:43 -0400

    feat: Add deleteUser().

--- vagrant/src/database_functions.py
+++ vagrant/src/database_functions.py
@@ -20,7 +20,7 @@ def connect(database_name="database"):
 
 # User Helper Functions
 def createUser(login_session):
-    """ creates a user using login information """
+    """ creates a user using login session information """
     db, cursor = connect()
     name = login_session.get('username')
     email = login_session.get('email')
@@ -28,11 +28,11 @@ def createUser(login_session):
     query = "INSERT INTO Users (name, email, picture) VALUES (%s,%s,%s);"
     param = (name, email, picture,)
     cursor.execute(query, param)
-    query = "SELECT id FROM Users WHERE email = %s"
+    query = "SELECT id FROM Users WHERE email = %s;"
     param = (email,)
     cursor.execute(query, param)
     user_id = cursor.fetchone()[0]
-    query = "INSERT INTO Permissions (user_id) VALUES (%s)"
+    query = "INSERT INTO Permissions (user_id) VALUES (%s);"
     param = (user_id,)
     cursor.execute(query, param)
     db.commit()
@@ -40,11 +40,29 @@ def createUser(login_session):
     return user_id
 
 
+def deleteUser(user_id):
+    """ Deletes user from users table and returns the users information """
+    try:
+        db, cursor = connect()
+        # Delete user permissions
+        query = "DELETE FROM Permissions WHERE user_id = %s;"
+        cursor.execute(query, user_id)
+        # Delete user from Users table
+        query = "DELETE FROM Users WHERE id = %s RETURNING *;"
+        cursor.execute(query, user_id)
+        user = cursor.fetchall()
+        db.commit()
+        db.close()
+        return user
+    except:
+        return None
+
+
 def getUserInfo(user_id):
     """ Returns user iformation for specified user """
     try:
         db, cursor = connect()
-        query = "SELECT * FROM Users WHERE id = %s"
+        query = "SELECT * FROM Users WHERE id = %s;"
         param = (user_id,)
         cursor.execute(query, param)
         user = cursor.fetchall()
@@ -58,7 +76,7 @@ def getUserID(email):
     """ Returns user id associated with given email """
     try:
         db, cursor = connect()
-        query = "SELECT id FROM Users WHERE email = %s"
+        query = "SELECT id FROM Users WHERE email = %s;"
         param = (email,)
         cursor.execute(query, param)
         user_id = cursor.fetchone()[0]
@@ -66,3 +84,5 @@ def getUserID(email):
         return user_id
     except:
         return None
+
+
